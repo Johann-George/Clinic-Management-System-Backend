7de47a1d8c7d575287144d1ca54f0f5564a4bbb4
Johann George <johanngeorge2003@gmail.com>
2025-09-02 12:53:18 +0530

    Implemented authentication and authorization

--- src/main/java/com/cms/backend/util/JwtUtil.java
+++ src/main/java/com/cms/backend/util/JwtUtil.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import com.cms.backend.constants.ApplicationConstants;
 import com.cms.backend.model.Patient;
+import com.cms.backend.model.Staff;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -30,10 +31,24 @@ public class JwtUtil {
 		String jwt = "";
 		String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY, ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
 		SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-		Patient fetchedPatient = (Patient) authentication.getPrincipal();
+		Object principal = authentication.getPrincipal();
+		String username = null;
+		String contactNo = null;
+
+		if(principal instanceof Patient) {
+			Patient patient = (Patient) principal;
+			username = patient.getName();
+			contactNo = patient.getContactNo();
+		}
+		else {
+			Staff staff = (Staff) principal;
+			username = staff.getName();
+			contactNo = staff.getContactNo();
+		}
+
 		jwt = Jwts.builder().issuer("Techno King").subject("JWT Token")
-				.claim("username", fetchedPatient.getName())
-				.claim("contactNumber", fetchedPatient.getContactNo())
+				.claim("username", username)
+				.claim("contactNumber", contactNo)
 				.claim("role", authentication.getAuthorities().iterator().next().getAuthority())
 				.issuedAt(new java.util.Date())
 				.expiration(new java.util.Date((new java.util.Date()).getTime() + 60 * 60 * 1000))
